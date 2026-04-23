01a8f20c88e6cbeba4cb786f77ba18287ab543d8
Doğa <dogabeey@gmail.com>
2026-04-24 01:38:24 +0300

    screen management

---
+++ Assets/Scripts/Management/GameScreen.cs
@@ -0,0 +1,30 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Management
+{
+    public abstract class GameScreen : SerializedMonoBehaviour
+    {
+        public ScreenID panelID;
+        public Animator animator;
+        public string playAnimationName;
+
+        private void OnValidate()
+        {
+            if (animator == null)
+            {
+                TryGetComponent(out animator);
+            }
+        }
+
+        public abstract void Show();
+    }
+
+    public enum ScreenID
+    {
+        WinScreen,
+        LoseScreen
+    }
+}
---
+++ Assets/Scripts/Management/ScreenManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Management
+{
+    public class ScreenManager : MonoBehaviour
+    {
+        internal List<GameScreen> screens = new List<GameScreen>();
+
+        public void Init()
+        {
+            screens.AddRange(FindObjectsOfType<GameScreen>(true));
+
+            //Show(firstScreen);
+        }
+
+        public void Show(GameScreen gameScreen)
+        {
+            screens.ForEach(screen => screen.gameObject.SetActive(false));
+            ShowScreen(gameScreen);
+        }
+
+        public void Show(ScreenID screenID)
+        {
+            screens.ForEach(screen => screen.gameObject.SetActive(false));
+            GameScreen gameScreen = screens.Find(screen => screen.panelID == screenID);
+            ShowScreen(gameScreen);
+        }
+        public void CloseAllScreens()
+        {
+            screens.ForEach(screen => screen.gameObject.SetActive(false));
+        }
+
+        private static void ShowScreen(GameScreen gameScreen)
+        {
+            gameScreen.gameObject.SetActive(true);
+            gameScreen.Show();
+        }
+    }
+
+}
